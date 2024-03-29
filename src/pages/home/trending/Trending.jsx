88bb35f77a1a90a4bab4f6bedeb4055d82bb9fa1
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-03-29 19:10:55 +0530

    refactor:moved mixins file inside src

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -2,11 +2,13 @@ import  { useState } from "react";
 
 import ContentWrapper from "../../../components/contentWrapper/contentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
 
 
 const Trending = () => {
     const [endpoint, setEndpoint] = useState("day");
-
+    const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
+console.log("lets see data",data)
 
     const onTabChange = (tab) => {
         setEndpoint(tab === "Day" ? "day" : "week");
