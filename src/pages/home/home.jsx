7e9f70385ba451ea01adbab9efc3c6ad5331eb8b
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-03-31 08:35:11 +0530

    feat:added Popular movies

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,15 @@
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import "./style.scss";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 const Home = () => {
   return (
     <div>
       <HeroBanner />
       <Trending />
+      <Popular/>
+      <TopRated/>
     </div>
   );
 };
