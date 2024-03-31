b9763b30ffa952d6cfab7039ffcff29593cd027d
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-03-31 11:15:52 +0530

    feat:added video section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,6 +4,7 @@ import "./style.scss";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "../../components/cast/Cast";
 import useFetch from "../../hooks/useFetch";
+import VideosSection from "./videosSection/VideosSection";
 
 const Details = () => {
   const { mediaType, id } = useParams();
@@ -17,6 +18,7 @@ const Details = () => {
     <div>
       <DetailsBanner />
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
     </div>
   );
 };
