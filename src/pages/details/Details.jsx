3963a3db6747e446616dd9e2a8ced229c05452c2
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-03-31 11:11:55 +0530

    feat:added top caste with error handling

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,11 +2,21 @@ import { useParams } from "react-router-dom";
 import "./style.scss";
 
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "../../components/cast/Cast";
+import useFetch from "../../hooks/useFetch";
 
 const Details = () => {
+  const { mediaType, id } = useParams();
+
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+      `/${mediaType}/${id}/credits`
+  );
+
   return (
     <div>
       <DetailsBanner />
+      <Cast data={credits?.cast} loading={creditsLoading} />
     </div>
   );
 };
